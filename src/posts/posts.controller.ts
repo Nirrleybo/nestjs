319cff6890852899b479de9e835a682f76af7c34
Nir Leibovitch <nir@giffgaff.co.uk>
2020-12-25 14:40:31 +0000

    Add posts module

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { PostsService } from './posts.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getHello(): string {
+    return this.postsService.getHello();
+  }
+  
+  @Get()
+  getAllPosts(): string {
+    return this.postsService.getHello();
+  }
+}
