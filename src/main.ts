76dd731cb6047c6e2973c3c6b9523aa5fd718441
Nir Leibovitch <nir@giffgaff.co.uk>
2020-12-26 22:31:01 +0000

    Add custom exceptions handeling

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { ExceptionsLoggerFilter } from './log/exceptionsLogger.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
+  const { httpAdapter } = app.get(HttpAdapterHost);
   
+  app.useGlobalFilters(new ExceptionsLoggerFilter(httpAdapter));
   app.use(cookieParser());
   await app.listen(port);
 }
